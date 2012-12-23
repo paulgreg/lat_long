3d7ad233394227db0e5f7716e303c84438b8480e
paulgreg <paulgreg@gmail.com>
2012-12-23 17:53:04 +0100

    Calculate point 5 km south, north, east and west

--- GeoBox.php
+++ GeoBox.php
@@ -1,12 +1,25 @@
 <?php
 
-const EARTH_RADIUS_AT_EQUATOR = 6378;
+const EARTH_RADIUS_AT_EQUATOR = 6378000; // In meter
 
 class GeoBox {
+
+    function GeoBox() {
+        $this->CIRCUMFERENCE_AT_EQUATOR = $this->getCircumference(0);
+    } 
 
     public function getCircumference($latitude) {
         return 2 * M_PI * cos(deg2rad($latitude)) * EARTH_RADIUS_AT_EQUATOR;
     }
+    public function getDeltaLatitude($latitude, $m) {
+        $delta = ($m / $this->CIRCUMFERENCE_AT_EQUATOR * 360);
+        return $latitude + $delta;
+    }
 
+    public function getDeltaLongitude($latitude, $longitude, $m) {
+        $circumference = $this->getCircumference($latitude);
+        $delta = ($m / $circumference * 360);
+        return $longitude + $delta;
+    }
 }
 ?>
--- GeoBoxTest.php
+++ GeoBoxTest.php
@@ -9,22 +9,41 @@ class GeoBoxTest extends PHPUnit_Framework_TestCase
         $this->gb = new GeoBox();
     }
 
-    public function test_circumference_at_equator()
-    {
+    public function test_circumference_at_equator() {
         $circumference = $this->gb->getCircumference(0.0);
-        $this->assertEquals(40074, round($circumference));
+        $this->assertEquals(40074156, round($circumference));
     }
 
-    public function test_circumference_at_lat_45()
-    {
+    public function test_circumference_at_lat_45() {
         $circumference = $this->gb->getCircumference(45);
-        $this->assertEquals(28337, round($circumference));
+        $this->assertEquals(28336707, round($circumference));
     }
 
-    public function test_circumference_at_lat_66()
-    {
+    public function test_circumference_at_lat_66() {
         $circumference = $this->gb->getCircumference(66);
-        $this->assertEquals(16300, round($circumference));
+        $this->assertEquals(16299628, round($circumference));
+    }
+
+    // Paris "2.353276,48.856078"
+
+    public function test_point_5_km_north() {
+        $latitude = $this->gb->getDeltaLatitude(48.856078, 5000);
+        $this->assertEquals(48.900995, round($latitude, 6));
+    }
+
+    public function test_point_5_km_south() {
+        $latitude = $this->gb->getDeltaLatitude(48.856078, -5000);
+        $this->assertEquals(48.811161, round($latitude, 6));
+    }
+
+    public function test_point_5_km_west() {
+        $longitude = $this->gb->getDeltaLongitude(48.856078, 2.353276, 5000);
+        $this->assertEquals(2.421543, round($longitude, 6));
+    }
+
+    public function test_point_5_km_east() {
+        $longitude = $this->gb->getDeltaLongitude(48.856078, 2.353276, -5000);
+        $this->assertEquals(2.285009, round($longitude, 6));
     }
 }
 ?>
